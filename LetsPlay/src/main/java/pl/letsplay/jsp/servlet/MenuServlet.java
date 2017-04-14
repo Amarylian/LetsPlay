76c09735452d4dd61ff7c9c6d7a828aca366a056
Karol Majka <karol.x.majka@gsk.com>
2017-04-14 18:52:26 +0200

    add security to menu move

--- LetsPlay/src/main/java/pl/letsplay/jsp/servlet/MenuServlet.java
+++ LetsPlay/src/main/java/pl/letsplay/jsp/servlet/MenuServlet.java
@@ -8,6 +8,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import pl.letsplay.beans.User;
+
 /**
  * Servlet implementation class menuServlet
  */
@@ -28,25 +30,39 @@ public class MenuServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("text/html; charset=UTF-8");
 		String button = request.getParameter("button");
+		User user = (User) request.getSession().getAttribute("user");
 		if(button == null) {
 			button = "";
 		}
+		
 		if(button.equals("Register")) {
-			RequestDispatcher rd=request.getRequestDispatcher("/register.jsp");  
-	    	rd.include(request,response); 
+			if(user != null) {
+				moveToSide(request, response, "/index.jsp");
+			} else {
+				moveToSide(request, response, "/register.jsp");
+			}
 		} else if(button.equals("CreateMeeting")) {
-			RequestDispatcher rd=request.getRequestDispatcher("/createMeeting.jsp");  
-	    	rd.include(request,response); 
+			if(user == null) {
+				moveToSide(request, response, "/index.jsp");
+			} else {
+				moveToSide(request, response, "/createMeeting.jsp");
+			}
 		} else if(button.equals("Login")) {
-			RequestDispatcher rd=request.getRequestDispatcher("/login.jsp");  
-	    	rd.include(request,response); 
+			if(user != null) {
+				moveToSide(request, response, "/index.jsp");
+			} else {
+				moveToSide(request, response, "/login.jsp");
+			}
 		} else if(button.equals("Logout")) {
 			request.getSession().setAttribute("user", null);
-			RequestDispatcher rd=request.getRequestDispatcher("/index.jsp");  
-	    	rd.include(request,response); 
+			moveToSide(request, response, "/index.jsp");
 		} else {
-			RequestDispatcher rd=request.getRequestDispatcher("/index.jsp");  
-	    	rd.include(request,response); 
+			moveToSide(request, response, "/index.jsp");
 		}
+	}
+	
+	private void moveToSide(HttpServletRequest request, HttpServletResponse response, String url) throws ServletException, IOException {
+		RequestDispatcher rd=request.getRequestDispatcher(url);  
+    	rd.include(request,response); 
 	}
 }
