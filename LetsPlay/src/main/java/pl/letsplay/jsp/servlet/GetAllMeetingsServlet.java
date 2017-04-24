792d79a6c66c564753adc0e8b9ba70521514e975
implikacja <agnieszka.l1995@gmail.com>
2017-04-24 18:19:17 +0200

    GetAllMeetingServlet

--- LetsPlay/src/main/java/pl/letsplay/jsp/servlet/GetAllMeetingsServlet.java
+++ LetsPlay/src/main/java/pl/letsplay/jsp/servlet/GetAllMeetingsServlet.java
@@ -1,10 +1,17 @@
 package pl.letsplay.jsp.servlet;
 
 import java.io.IOException;
+import java.sql.SQLException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import pl.letsplay.beans.Meeting;
+import pl.letsplay.beans.User;
+import pl.letsplay.utils.DBUtils;
 
 /**
  * Servlet implementation class GetAllMeetingsServlet
@@ -20,13 +27,36 @@ public class GetAllMeetingsServlet extends HttpServlet {
         // TODO Auto-generated constructor stub
     }
 
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		doGet(request, response);
+        response.setContentType("text/html; charset=UTF-8");
+		String button = request.getParameter("button");
+		Meeting m = null;
+		try {
+			m = DBUtils.findMeeting(button);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		if(m != null) {
+	    	RequestDispatcher rd=request.getRequestDispatcher("getAllMeetings.jsp");  
+	    	request.getSession().setAttribute("meeting", m);
+	    	rd.include(request,response); 
+	    } else {
+	    	RequestDispatcher rd=request.getRequestDispatcher("index.jsp");  
+	    	rd.include(request,response);
+	    }
 	}
 
 }
