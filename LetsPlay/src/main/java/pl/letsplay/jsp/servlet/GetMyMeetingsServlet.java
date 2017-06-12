b8c24d3c613be4ecfb265503afb7d897e06fe7f0
Karol Majka <karol.x.majka@gsk.com>
2017-06-12 16:04:08 +0200

    createMeeting

--- LetsPlay/src/main/java/pl/letsplay/jsp/servlet/GetMyMeetingsServlet.java
+++ LetsPlay/src/main/java/pl/letsplay/jsp/servlet/GetMyMeetingsServlet.java
@@ -1,10 +1,14 @@
 package pl.letsplay.jsp.servlet;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import pl.letsplay.beans.Meeting;
 
 /**
  * Klasa implementująca Servlet do obsługi wyświetlania  spotkań zalogowanego użytkownika
@@ -37,4 +41,20 @@ public class GetMyMeetingsServlet extends HttpServlet {
 		doGet(request, response);
 	}
 
+	/**
+	 * Meeting meeting = (Meeting)request.getSession().getAttribute("meeting");
+	 * funkcja przekierwouje do tworzenia kolejnej edycji spotkania
+	 */
+	protected void createMeeting(Meeting meeting, HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		Meeting createMeeting = new Meeting(-1, false, meeting.getCity(), null, null, meeting.getAddress(), false, 0, meeting.getMaxNumber(), meeting.getAttentions());
+		request.getSession().setAttribute("CreateMeeting", createMeeting);
+		this.moveToSide(request, response, "createMeeting.jsp");
+	}
+	/**
+	 * funkcja pomocnicza do przekierowania na inną stronę
+	 */
+	private void moveToSide(HttpServletRequest request, HttpServletResponse response, String url) throws ServletException, IOException {
+		RequestDispatcher rd=request.getRequestDispatcher(url);  
+    	rd.include(request,response); 
+	}
 }
